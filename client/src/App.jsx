04b88072104e105e660cf7564ddfe68b91ec7c65
Syedshara <syedsharashree@gmail.com>
2024-03-09 23:11:07 +0530

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import DashBoard from './pages/DashBoard';
 import Projects from './pages/Projects';
+import Header from './pages/Header';
 
 const App = () => {
   return (
    <BrowserRouter>
+       <Header/>
        <Routes>
           <Route  path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
